826c1ed7e551a93ea125db45a77dff6691c6e6f2
Ivan Oparin <oparin@me.com>
2025-11-19 21:29:48 +0400

    Remove router backports

--- Package.swift
+++ Package.swift
@@ -18,14 +18,12 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/ivalx1s/darwin-relux.git", from: "8.1.0"),
-        .package(url: "https://github.com/johnpatrickmorgan/NavigationBackport.git", from: "0.9.3"),
 	],
 	targets: [
 		.target(
 			name: "ReluxRouter",
 			dependencies:  [
 				.product(name: "Relux", package: "darwin-relux"),
-                .product(name: "NavigationBackport", package: "NavigationBackport"),
 			],
 			path: "Sources"
 		),
