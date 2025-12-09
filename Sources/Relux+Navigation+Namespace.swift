a0d59708792e7fea124c26c73a6e26cf10482c4f
Ivan Oparin <oparin@me.com>
2025-12-09 21:43:15 +0400

    Extract navigation abstracting protocols to core relux lib

--- Sources/Relux+Navigation+Namespace.swift
+++ Sources/Relux+Navigation+Namespace.swift
@@ -1,23 +1,2 @@
 import Relux
 
-extension Relux {
-    public enum Navigation {}
-}
-
-extension Relux.Navigation {
-    public protocol RouterProtocol: Relux.HybridState { }
-    public protocol PathComponent: Equatable, Hashable, Sendable {}
-    public protocol PathCodableComponent: PathComponent, Codable {}
-    
-    public protocol ModalComponent: PathComponent, Identifiable {}
-    public protocol ModalCodableComponent: ModalComponent, Codable, Identifiable {}
-}
-
-public extension Relux.Navigation.ModalComponent {
-    var id: Int { self.hashValue }
-}
-
-
-public extension Relux.Navigation.ModalCodableComponent {
-    var id: Int { self.hashValue }
-}
